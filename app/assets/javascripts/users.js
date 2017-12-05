9c7572194e685b65e34fadf625a5546ccc766411
Taj Calles <tajcalles@gmail.com>
2017-12-04 18:31:26 -0700

    working list edit and del

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,40 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// $(document).ready(function() {
+//   $.ajax({
+//       url: 'http://json-server.devpointlabs.com/api/v1/users',
+//       type: 'GET',
+//       dataType: 'JSON'
+//     }).done(function(users) {
+//       console.log(users);
+//     }).fail(function(users) {
+//       console.log(users);
+//     });
+//   });
+//
+// $('.user-list').click(function() {
+//
+//   $.ajax({
+//       url: 'http://json-server.devpointlabs.com/api/v1/users',
+//       type: 'GET',
+//       dataType: 'JSON'
+//     }).done(function(users) {
+//
+//     }).fail(function(users) {
+//       console.log(users);
+//     });
+//   });
+  $(document).ready(function () {
+    $.ajax({
+      url: 'http://json-server.devpointlabs.com/api/v1/users',
+      method: 'GET',
+      dataType: 'JSON'
+    }).done(function (users) {
+      users.forEach(function (user) {
+        var list = $('.user-list')
+        var li = '<li data-user-id="' + user.id + '"><strong>' + user.first_name +
+        ' - ' + user.last_name + '</strong>: ' + user.phone_number + '</li>' +
+        '<button class="btn blue edit-button">Edit</button>' +
+        '<button class="btn red delete-button">Delete</button>'
+        list.append(li)
+      });
+    });
+  });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,5 @@
 class UsersController < ApplicationController
+
   def index
-  end
-
-  def new
-  end
-
-  def show
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
