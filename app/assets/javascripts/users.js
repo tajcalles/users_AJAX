8fca1535594fcb330c1982223708f7f4b2fb7d2e
Taj Calles <tajcalles@gmail.com>
2017-12-05 08:37:43 -0700

    working list no post :(

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,40 +1,28 @@
-// $(document).ready(function() {
-//   $.ajax({
-//       url: 'http://json-server.devpointlabs.com/api/v1/users',
-//       type: 'GET',
-//       dataType: 'JSON'
-//     }).done(function(users) {
-//       console.log(users);
-//     }).fail(function(users) {
-//       console.log(users);
-//     });
-//   });
-//
-// $('.user-list').click(function() {
-//
-//   $.ajax({
-//       url: 'http://json-server.devpointlabs.com/api/v1/users',
-//       type: 'GET',
-//       dataType: 'JSON'
-//     }).done(function(users) {
-//
-//     }).fail(function(users) {
-//       console.log(users);
-//     });
-//   });
-  $(document).ready(function () {
-    $.ajax({
-      url: 'http://json-server.devpointlabs.com/api/v1/users',
-      method: 'GET',
-      dataType: 'JSON'
-    }).done(function (users) {
-      users.forEach(function (user) {
-        var list = $('.user-list')
-        var li = '<li data-user-id="' + user.id + '"><strong>' + user.first_name +
-        ' - ' + user.last_name + '</strong>: ' + user.phone_number + '</li>' +
-        '<button class="btn blue edit-button">Edit</button>' +
-        '<button class="btn red delete-button">Delete</button>'
-        list.append(li)
-      });
+$(document).ready(function () {
+  $.ajax({
+    url: 'http://json-server.devpointlabs.com/api/v1/users',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done(function (users) {
+    users.forEach(function (user) {
+      var list = $('.user-list')
+      var li = '<li data-user-id="' + user.id + '"><strong>' + user.first_name +
+      ' - ' + user.last_name + '</strong>: ' + user.phone_number + '</li>' +
+      '<button class="btn blue edit-button">Edit</button>' +
+      '<button class="btn red delete-button">Delete</button>'
+      list.append(li)
     });
   });
+});
+
+$('.join-group').click(function() {
+  $.ajax({
+    url: 'http://json-server.devpointlabs.com/api/v1/users',
+    method: 'POST',
+    dataType: 'JSON'
+  }).done(function(great) {
+    console.log(great)
+  }).fail(function (bad) {
+      console.log(bad)
+  });
+});
